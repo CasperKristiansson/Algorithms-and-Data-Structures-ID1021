059ad82d2bf2527ea355ee1160acf1a8336057d6
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-03-23 18:05:47 +0100

    Removed old exams

---
+++ Lab4/BreadthFirstDirectPaths.java
@@ -0,0 +1,101 @@
+/**
+ * A class that implements the BreadthFirstSearch algorithm
+ */
+public class BreadthFirstDirectPaths {
+    private boolean[] marked;   // Does it have a path to start?
+    private int[] edgeTo;       // last vertex on known path to this vertex
+    private int[] distTo;       // length of shortest known path to this vertex
+
+    /**
+     * Constructor to initialize the algorithm
+     * 
+     * @param G the graph
+     * @param start the source vertex
+     */
+    public BreadthFirstDirectPaths(DirectGraph G, int start) {
+        marked = new boolean[G.V()];
+        distTo = new int[G.V()];
+        edgeTo = new int[G.V()];
+        validateVertex(start);
+        bfs(G, start);
+    }
+
+    /**
+     * Performs the breadth-first search algorithm by first setting
+     * the default distance to infinity for all vertices and then
+     * setting the distance to the source vertex to 0. We than use a queue
+     * to iterate through the vertices and count the distance to each one.
+     * 
+     * @param G the graph
+     * @param start the source vertex
+     */
+    private void bfs(DirectGraph G, int start) {
+        Queue<Integer> queue = new Queue<Integer>();
+
+        for (int v = 0; v < G.V(); v++) distTo[v] = Integer.MAX_VALUE;
+        distTo[start] = 0;
+        marked[start] = true;
+        queue.enqueue(start);
+
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns if it exist a path from start to a given vertex
+     * 
+     * @param v the vertex
+     * @return true if it exist a path
+     */
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    /**
+     * Returns the number of edges in a path between start and a given vertex
+     * 
+     * @param v the vertex
+     * @return the number of edges in a path
+     */
+    public int distTo(int v) {
+        validateVertex(v);
+        return distTo[v];
+    }
+
+    /**
+     * Returns the path between start and a given vertex
+     * 
+     * @param v the vertex
+     * @return the path
+     */
+    public Iterable<Integer> pathTo(int v) {
+        validateVertex(v);
+        if (!hasPathTo(v)) return null;
+        
+        Stack<Integer> path = new Stack<Integer>();
+        int x;
+        for (x = v; distTo[x] != 0; x = edgeTo[x]) path.push(x);
+        path.push(x);
+        return path;
+    }
+
+    /**
+     * Validates the vertex
+     * 
+     * @throws IllegalArgumentException if the vertex is invalid
+     * @param v the vertex
+     */
+    private void validateVertex(int v) {
+        if (v < 0 || v >= marked.length) throw new IllegalArgumentException("Out of bounds");
+    }
+}
---
+++ Lab4/L4Uppgift5.java
@@ -0,0 +1,35 @@
+/**
+ * @author Casper Kristiansson
+ * Code Generated: 2021-10-10
+ * Code Updated: 2021-10-10
+ * Problem: Search for a path between A and B using DFS with directed edges
+ * Sources: Algorithms 4th edition (Directed Graphs, 4.2), https://algs4.cs.princeton.edu/40graphs/
+*/
+public class L4Uppgift5 {
+    /**
+     * A test method for the DFS algorithm. We first input a file and a start and end position.
+     * This algorithm uses directed graphs when trying to find a path between the start and end position.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String filename = "data.txt";
+        String from = "AL";
+        String to = "WV";
+
+        DirectSymbolGraph sg = new DirectSymbolGraph(filename); 
+        DirectGraph G = sg.graph();
+
+        int start = sg.indexOf(from);
+        int end = sg.indexOf(to);
+        BreadthFirstDirectPaths bfs = new BreadthFirstDirectPaths(G, start);
+
+        if (bfs.hasPathTo(end)) {
+            System.out.println(from + " to " + to);
+            for(int x : bfs.pathTo(end)) System.out.println("-" + sg.nameOf(x));
+        }
+        else {
+            System.out.println(from + " to " + to + " not connected ");
+        }
+    }
+}
